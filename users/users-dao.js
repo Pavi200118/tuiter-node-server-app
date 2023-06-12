9b5bb9b1f5aa42b9dda19315828b6328bc72aebb
Pavi200118 <pavisowmi01@gmail.com>
2023-06-12 12:19:52 -0400

    a5

--- users/users-dao.js
+++ users/users-dao.js
@@ -1,48 +1,50 @@
-import { v4 as uuidv4 } from 'uuid';
-let users = [];
+
+/* let users = []; */
+import people from './users.js'
+
+let users = people
 
 
 export const findAllUsers = () => users;
 
 
 export const findUserById = (uid) => {
- const index = users.findIndex((u) => u._id === uid);
- if (index !== -1) return users[index];
- return null;
+    const index = users.findIndex((u) => u._id === uid);
+    if (index !== -1) return users[index];
+    return null;
 };
 
 
 export const findUserByUsername = (username) => {
- const index = users.findIndex((u) => u.username === username);
- if (index !== -1) return users[index];
- return null;
+    const index = users.findIndex((u) => u.username === username);
+    if (index !== -1) return users[index];
+    return null;
 };
 
 
-  export const findUserByCredentials = (username, password) => {
-      const user = users.find((u) => u.username === username && u.password === password);
-      return user || null;
-    };
-
+export const findUserByCredentials = (username, password) => {
+    const index = users.findIndex((u) => u.username === username && u.password === password);
+    if (index !== -1) return users[index];
+    return null;
+};
 
 
 export const createUser = (user) => {
-const newUser = { ...user, _id:  uuidv4() };
-  users.push(newUser);
-  return newUser;
-  };
+    const newUser = {...user, _id: (new Date()).getTime() + ''}
+    users.push(newUser);
+    return newUser;
+}
 
 
 export const updateUser = (uid, user) => {
- const index = users.findIndex((u) => u._id === uid);
- users[index] = { ...users[index], ...user };
- return {status: 'ok'}
+    const index = users.findIndex((u) => u._id === uid);
+    users[index] = { ...users[index], ...user };
+    return users[index] 
 };
 
 
 export const deleteUser = (uid) => {
- const index = users.findIndex((u) => u._id === uid);
- users.splice(index, 1);
- return {status: 'ok'}
+    const index = users.findIndex((u) => u._id === uid);
+    users.splice(index, 1);
+    return { status: 'ok' }
 };
-
