a885a91a22ee46bcc32efed474e3f108c4251f28
Pavi200118 <pavisowmi01@gmail.com>
2023-06-12 12:06:20 -0400

    a5

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-import express from 'express';
-import session from "express-session";
-import cors from 'cors';
-import AuthController from "./users/auth-controller.js";
-import HelloController from "./controllers/hello-controller.js";
-import UserController from "./users/users-controller.js";
+import express from 'express'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors';
+import session from "express-session";
+import AuthController from "./users/auth-controller.js";
 
 const app = express();
 app.use(
-  session({
-    secret: "any string",
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        secret: "any string",
+        resave: false,
+        saveUninitialized: true,
+    })
 );
+
+app.use((req, res, next) => {
+    const allowedOrigins = ["http://localhost:3000", "https://pavithrasowmiyan-cs5610-a4.netlify.app"];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 app.use(express.json());
-app.use(
-cors({
-   credentials: true,
-   origin: "https://pavithrasowmiyan-cs5610-a4.netlify.app",
-})
-);
-
-
+const port = process.env.PORT || 4000;
 TuitsController(app);
 HelloController(app);
 UserController(app);
 AuthController(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
